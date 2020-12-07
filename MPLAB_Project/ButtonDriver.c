e27e3e6adc24003f040d5ff4f200a1d8b668806a
Lincoln O'Connell <20270879erc@gmail.com>
2020-12-07 11:47:25 +0000

    Added Button Driver header & source

---
+++ MPLAB_Project/ButtonDriver.c
@@ -0,0 +1,34 @@
+#include "buttons.h"
+#include <xc.h>
+
+void initialiseButtons() {
+        
+        ADCON1 = 0x07;
+        TRISA = 0xFF;
+        TRISB = 0xFF;
+}
+
+int* checkButtons() {
+    initialiseButtons();
+    int result[8] = { 0 };
+
+    unsigned int mask;
+    int i = 0;
+
+    for (mask = 1; mask != 16; mask <<= 1) {
+
+        if (PORTB & mask) {
+            result[i] = RIGHT_SIDE_HIGH;
+        }
+        i++;
+    }
+    
+    for (mask = 1; mask != 16; mask <<= 1) {
+
+        if (PORTA & mask) {
+            result[i] = RIGHT_SIDE_HIGH;
+        }
+        i++;
+    }
+    return result;
+}
---
+++ MPLAB_Project/ButtonDriver.h
@@ -0,0 +1,7 @@
+#ifndef BUTTONS_H
+#define	BUTTONS_H
+#define RIGHT_SIDE_HIGH 1
+
+void initialiseButtons();
+int* checkButtons();
+#endif
