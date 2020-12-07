1834716f379894a5b1a0dd2c199150cfca399e39
Lincoln O'Connell <20270879erc@gmail.com>
2020-12-07 14:52:18 +0000

    Added to heaterconfig

--- MPLAB_Project/HeaterConfigManager.c
+++ MPLAB_Project/HeaterConfigManager.c
@@ -1,26 +1,39 @@
-
 #include "HeaterConfigManager.h"
-
-
-  
-void SetTriggerTemp(double temp)
-{
-    
-}
-
-double GetTriggerTemp()
-{
-    
-}
 
 void IncrementMode()
 {
-    
+    heater.mode++;
 }
 
 void DecrementMode()
 {
+    heater.mode--;
+}
+
+void IncrementTemp()
+{
+    heater.displayTemp++;
+}
+
+void DecrementTemp()
+{
+    heater.displayTemp--;
+}
+  
+void SetTriggerTemp()
+{
+    double temp;
+    temp = heater.displayTemp;
     
+    if (temp <  - 50 || temp > 150)
+        heater.error = "Trigger Temp Not Between -50 and 150 degrees.";
+    else
+        heater.triggerTemp = temp;
+}
+
+double GetTriggerTemp()
+{
+    return heater.triggerTemp;
 }
 
 void SetHotTimer(int time)
@@ -28,7 +41,7 @@ void SetHotTimer(int time)
     if (time < 0  || time > 90)
         heater.hotTimer = time;
     else
-        heater.error = "Hot timer not between 0 and 90 seconds."
+        heater.error = "Hot timer not between 0 and 90 seconds.";
 }
 
 int GetHotTimer()
@@ -41,7 +54,7 @@ void SetColdTimer(int time)
     if (time < 0  || time > 90)
         heater.hotTimer = time;
     else
-        heater.error = "Cold timer not between 0 and 60 seconds."
+        heater.error = "Cold timer not between 0 and 60 seconds.";
 }
 
 int GetColdTimer()
