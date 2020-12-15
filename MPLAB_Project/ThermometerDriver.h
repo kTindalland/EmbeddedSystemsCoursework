67cfe27379b57e0cf1a10b26a060a9e59440c1f9
Kai Tindall <kaitindall2000@gmail.com>
2020-12-15 18:34:11 +0000

    Bug fix

--- MPLAB_Project/ThermometerDriver.h
+++ MPLAB_Project/ThermometerDriver.h
@@ -5,7 +5,9 @@
  * Created on 14 December 2020, 10:56
  */
 
+#ifndef __UNIT_TESTING
 #include <xc.h>
+#endif
 
 #ifndef DS18B20_H
 #define	DS18B20_H
@@ -13,12 +15,13 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
-    
+
+#ifndef __UNIT_TESTING
 #define DQ RA0
 #define DQ_TRIS TRISA0
 #define DQ_HIGH() DQ_TRIS = 1; 
 #define DQ_LOW() DQ_TRIS = 0; DQ = 0;
-    
+#endif
 
 
     void ThermInit();
