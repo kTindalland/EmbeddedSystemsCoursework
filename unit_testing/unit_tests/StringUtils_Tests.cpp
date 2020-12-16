1985f124152a0fe7c06f0e3951e0fc7a4dc2fcd1
Lincoln O'Connell <20270879erc@gmail.com>
2020-12-16 18:26:21 +0000

    Unit Tests for StringUtils started

---
+++ unit_testing/unit_tests/StringUtils_Tests.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+/* Include any code headers that need testing. */
+#include <StringUtils.h>
+
+TEST(StringLengthTests, sunnyDay) {
+	// Arrange
+	int result;
+	char twoLength[2] = {'A', 'B'};
+		
+	// Act
+	result = StringLength(twoLength);
+
+	// Assert
+	ASSERT_EQ(result, 2);
+}
+
+TEST(StringLengthTests, lowerRange) {
+	// Arrange
+	int result;
+	char noLength[];
+		
+	// Act
+	result = StringLength(noLength);
+
+	// Assert
+	ASSERT_EQ(result, 0);
+}
