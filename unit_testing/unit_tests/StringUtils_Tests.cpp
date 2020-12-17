6690b9711ee9d71f2f1fa6c2e55032825fead2b4
Lincoln O'Connell <20270879erc@gmail.com>
2020-12-17 17:16:29 +0000

    StringUtils ConvertDate UnitTests

--- unit_testing/unit_tests/StringUtils_Tests.cpp
+++ unit_testing/unit_tests/StringUtils_Tests.cpp
@@ -312,7 +312,7 @@ TEST(ConvertDateToStringTests, sunnyDay){
 	int resultNo = ConvertDateToString(result, date, month, year, 10);
 
 	// Assert
-	EXPECT_EQSTR(result, "19/04/2093");
+	EXPECT_STREQ(result, "19/04/2093");
 	ASSERT_EQ(resultNo, 1);
 }
 
@@ -327,7 +327,7 @@ TEST(ConvertDateToStringTests, sunnyDayDoubleDigitMonth){
 	int resultNo = ConvertDateToString(result, date, month, year, 10);
 
 	// Assert
-	EXPECT_EQSTR(result, "19/11/2093");
+	EXPECT_STREQ(result, "19/11/2093");
 	ASSERT_EQ(resultNo, 1);
 }
 
