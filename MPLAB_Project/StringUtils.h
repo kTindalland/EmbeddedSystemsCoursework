a17ec7cdaa4dbce8e442681a8e2f06284123262a
Lincoln O'Connell <20270879erc@gmail.com>
2020-12-17 13:18:49 +0000

    StringUtils Concatenate Testing

--- MPLAB_Project/StringUtils.h
+++ MPLAB_Project/StringUtils.h
@@ -6,8 +6,8 @@ extern "C" {
 #endif
 
 int StringLength(char s[]);
-void StringCopy(char from[], char to[]);
-void Concatenate(char sA[], char sB[], char result[]);
+int StringCopy(char from[], char to[]);
+int Concatenate(char sA[], char sB[], char result[]);
 void ConvertTime12ToString(char* result, int secs, int mins, int hours, int pm);
 void ConvertTime24ToString(char* result, int secs, int mins, int hours);
 void ConvertDateToString(char* result, int date, int month, int year);
