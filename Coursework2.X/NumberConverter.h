dcc9b16fee1a3b8385a43f2f5ced5af1ca0eb251
Kai Tindall <kaitindall2000@gmail.com>
2020-12-28 15:38:47 +0000

    Fixed number converter header file

--- Coursework2.X/NumberConverter.h
+++ Coursework2.X/NumberConverter.h
@@ -3,8 +3,6 @@
 
 char nbrcnvt_convert_digit(unsigned short digit);
 void nbrcnvt_convert_integer(signed short number, char* output);
-void nbrcnvt_convert_double(double number, char* output);
-int nbrcnvt_check_length_int(int number);
-int nbrcnvt_check_length_double(double number);
+unsigned char nbrcnvt_check_length_int(signed short number);
 
 #endif
